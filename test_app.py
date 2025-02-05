5b1c6813aba0a8f8115baa7086c5d5dc85213553
dev-ops-shashank <dev-ops-shashank@users.noreply.github.com>
2025-02-05 14:16:11 +0000

    style: auto-format code with Black

--- test_app.py
+++ test_app.py
@@ -2,11 +2,13 @@
 import pytest
 from app import app
 
+
 @pytest.fixture
 def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
+
 
 def test_health_check(client):
     rv = client.get("/health")
